9d7e11a28dfb8a2ca3b8e43bc7564efaf45df2ff
Alan Smith <a.bax.smith@gmail.com>
2015-08-15 11:43:54 -0600

    fully functional thing. yay! still needs help with transitions

--- public/scripts/index.js
+++ public/scripts/index.js
@@ -2,7 +2,7 @@ var ReactCSSTransitionGroup = React.addons.CSSTransitionGroup;
 
 var Content = React.createClass({
   getInitialState:  function() {
-    return{renderPanelGroup: false, renderStartButton: true};
+    return{renderStartButton: true, renderPanelGroup: false};
   },
   togglePanel: function(data) {
     this.setState({renderStartButton: data.display});
@@ -17,167 +17,6 @@ var Content = React.createClass({
           <TitlePanel/>
           <StartButton display={this.state.renderStartButton} onClick={this.togglePanel}/>
           <PanelGroup display={this.state.renderPanelGroup}/>
-        </div>
-      </div>
-    )
-  }
-});
-
-var StartButton = React.createClass({
-  handleClick: function(event) {
-    this.props.onClick({start: true, display: false});
-  },
-  render: function() {
-    if (this.props.display === true) {
-      return (
-        <ReactCSSTransitionGroup transitionName="example" transitionAppear={true}>
-          <button id='btn-start' onClick={this.handleClick} className='btn btn-lg btn-block'>Get Started!</button>
-        </ReactCSSTransitionGroup>
-      )
-    } else {
-      return (false);
-    }
-  }
-})
-
-var PanelGroup = React.createClass({
-  getInitialState: function() {
-    return{date: "", time: "", info: {name: "", phone: "", email: ""}}
-  },
-  handleDate: function(data) {
-    this.setState({date: data.date})
-  },
-  handleTime: function(data) {
-    this.setState({time: data.time})
-  },
-  handleInfo: function(data) {
-    this.setState({ info: { name: data.name, phone: data.phone, email: data.email }})
-  },
-  render: function() {
-    if(this.props.display === true) {
-      return(
-        <ReactCSSTransitionGroup transitionName="example" transitionAppear={true}>
-          <div className='panel-group' id='accordion' role='tablist' aria-multiselectable='true'>
-            <CalendarPanel onDateClick={this.handleDate} onTimeClick={this.handleTime}/>
-            <InfoPanel onClick={this.handleInfo} />
-            <ConfirmPanel date={this.state.date} time={this.state.time} info={this.state.info} />
-          </div>
-        </ReactCSSTransitionGroup>
-    )} else {
-      return(false);
-    }
-  }
-});
-
-var CalendarPanel = React.createClass({
-  getInitialState: function() {
-    return{activeDate: undefined}
-  },
-  handleDateClick: function(date) {
-    console.log();
-    this.props.onDateClick({date: date});
-  },
-  handleTimeClick: function(time) {
-    this.props.onTimeClick({time: time});
-  },
-  render: function() {
-    return (
-      <div className='panel panel-default'>
-        <a id='calendar-panel' data-toggle='collapse' data-parent='#accordion' href='#collapseOne' aria-expanded='true' aria-controls='collapseOne'>
-          <div className='panel-heading' role='tab' id='calendar'>
-            <h1 className='panel-title'>Calendar</h1>
-          </div>
-        </a>
-        <div id='collapseOne' className='panel-collapse collapse' role='tabpanel' aria-labelledby='calendar'>
-          <div className='panel-body'>
-            <h6 className='text-muted calendar-heading'>DATES</h6>
-            <div className='col-xs-1'><div onClick={this.handleDateClick.bind(this, "Sunday")} className='thumbnail dates' id="sunday">SUN</div></div>
-            <div className='col-xs-1'><div onClick={this.handleDateClick.bind(this, "Monday")} className='thumbnail dates'>MON</div></div>
-            <div className='col-xs-1'><div onClick={this.handleDateClick.bind(this, "Tuesday")} className='thumbnail dates'>TUE</div></div>
-            <div className='col-xs-1'><div onClick={this.handleDateClick.bind(this, "Wednesday")} className='thumbnail dates'>WED</div></div>
-            <div className='col-xs-1'><div onClick={this.handleDateClick.bind(this, "Thursday")} className='thumbnail dates'>THR</div></div>
-            <div className='col-xs-1'><div onClick={this.handleDateClick.bind(this, "Friday")} className='thumbnail dates'>FRI</div></div>
-            <div className='col-xs-1'><div onClick={this.handleDateClick.bind(this, "Saturday")} className='thumbnail dates'>SAT</div></div>
-            <TimeOptions onClick={this.handleTimeClick}/>
-          </div>
-        </div>
-      </div>
-    )
-  }
-});
-
-var TimeOptions = React.createClass({
-  handleClick: function(time) {
-    this.props.onClick({time: time});
-  },
-  render: function() {
-    return (
-      <div>
-        <h6 className='text-muted calendar-heading'>AVAILABILITY</h6>
-        <div className='col-xs-3'><div onClick={this.handleClick.bind(this, "8:00-9:00 AM")} className="thumbnail times">8-9 AM</div></div>
-        <div className='col-xs-3'><div onClick={this.handleClick.bind(this, "9:00-10:00 AM")} className="thumbnail times">9-10 AM</div></div>
-        <div className='col-xs-3'><div onClick={this.handleClick.bind(this, "10:00-11:00 AM")} className="thumbnail times">10-11 AM</div></div>
-        <div className='col-xs-3'><div onClick={this.handleClick.bind(this, "11:00-12:00 NOON")} className="thumbnail times">11-12 PM</div></div>
-        <div className='col-xs-3'><div onClick={this.handleClick.bind(this, "1:00-2:00 PM")} className="thumbnail times">1-2 PM</div></div>
-        <div className='col-xs-3'><div onClick={this.handleClick.bind(this, "2:00-3:00 PM")} className="thumbnail times">2-3 PM</div></div>
-        <div className='col-xs-3'><div onClick={this.handleClick.bind(this, "3:00-4:00 PM")} className="thumbnail times">3-4 PM</div></div>
-        <div className='col-xs-3'><div onClick={this.handleClick.bind(this, "4:00-5:00 PM")} className="thumbnail times">4-5 PM</div></div>
-      </div>
-    )
-  }
-});
-
-
-var InfoPanel = React.createClass({
-  handleInfo: function(event) {
-    event.preventDefault();
-    var name = React.findDOMNode(this.refs.name).value.trim();
-    var phone = React.findDOMNode(this.refs.phone).value.trim();
-    var email = React.findDOMNode(this.refs.email).value.trim();
-    this.props.onClick({name: name, phone: phone, email: email})
-  },
-
-  render: function() {
-    return (
-      <div className='panel panel-default'>
-        <a id='info-panel' data-toggle='collapse' data-parent='#accordion' href='#collapseTwo' aria-expanded='true' aria-controls='collapseOne'>
-          <div className='panel-heading' role='tab' id='calendar'>
-            <h1 className='panel-title'>Your Info</h1>
-          </div>
-        </a>
-        <div id='collapseTwo' className='panel-collapse collapse' role='tabpanel' aria-labelledby='calendar'>
-          <div className='panel-body'>
-            <p className='text-muted'>Please add your info.</p>
-            <div className='form-horizontal'>
-              <input id='name' className='form-control' ref='name' placeholder='Full name'></input>
-              <input id='phone' className='form-control' ref='phone' placeholder='Phone number'></input>
-              <input id='email' className='form-control' ref='email' placeholder='Email address'></input>
-              <button id='info-btn' className='btn btn-block' onClick={this.handleInfo}>Next</button>
-            </div>
-          </div>
-        </div>
-      </div>
-    )
-  }
-});
-
-var ConfirmPanel = React.createClass({
-  render: function() {
-    return (
-      <div className='panel panel-default'>
-        <a id='info-panel' data-toggle='collapse' data-parent='#accordion' href='#collapseThree' aria-expanded='true' aria-controls='collapseOne'>
-          <div className='panel-heading' role='tab' id='calendar'>
-            <h1 className='panel-title'>Confirmation</h1>
-          </div>
-        </a>
-        <div id='collapseThree' className='panel-collapse collapse' role='tabpanel' aria-labelledby='calendar'>
-          <div className='panel-body' align='center'>
-            <h4>Thanks {this.props.info.name}</h4>
-            <p className='text-muted'>We have you set for {this.props.date} at {this.props.time}</p>
-            <p>{this.props.info.phone}</p>
-            <p>{this.props.info.email}</p>
-            <button id="confirm-btn" className="btn btn-block">Confirm?</button>
-          </div>
         </div>
       </div>
     )
@@ -213,7 +52,225 @@ var TitlePanel = React.createClass({
       </div>
     )
   }
+});
+
+var StartButton = React.createClass({
+  handleClick: function(event) {
+    this.props.onClick({start: true, display: false});
+  },
+  render: function() {
+    if (this.props.display === true) {
+      return (
+        <ReactCSSTransitionGroup transitionName="example" transitionAppear={true}>
+          <button id='btn-start' onClick={this.handleClick} className='btn btn-lg btn-block'>Get Started!</button>
+        </ReactCSSTransitionGroup>
+      )
+    } else {
+      return (false);
+    }
+  }
 })
+
+var PanelGroup = React.createClass({
+  getInitialState: function() {
+    return{ date: "__________",
+            time: "__________",
+            info: { name: "",
+                    phone: "",
+                    email: ""
+                  },
+            availableDates: [ {'displayDay':'SUN', 'day':'Sunday', 'date': ''},
+                              {'displayDay':'MON', 'day':'Monday', 'date': ''},
+                              {'displayDay':'TUE', 'day':'Tuesday', 'date': ''},
+                              {'displayDay':'WED', 'day':'Wednesday', 'date': ''},
+                              {'displayDay':'THR', 'day':'Thursday', 'date': ''},
+                              {'displayDay':'FRI', 'day':'Friday', 'date': ''},
+                              {'displayDay':'SAT', 'day':'Saturday', 'date': ''}
+                            ],
+            availableTimes: [ {'timeSlot':'8:00-9:00 AM', 'displayTime': '8-9 AM'},
+                              {'timeSlot':'9:00-10:00 AM', 'displayTime': '9-10 AM'},
+                              {'timeSlot':'10:00-11:00 AM', 'displayTime': '10-11 AM'},
+                              {'timeSlot':'11:00-12:00 NOON', 'displayTime': '11-12 PM'},
+                              {'timeSlot':'1:00-2:00 PM', 'displayTime': '1-2 PM'},
+                              {'timeSlot':'2:00-3:00 PM', 'displayTime': '2-3 PM'},
+                              {'timeSlot':'3:00-4:00 PM', 'displayTime': '3-4 PM'},
+                              {'timeSlot':'4:00-5:00 PM', 'displayTime': '4-5 PM'}
+                            ]
+          }
+  },
+  handleDate: function(data) {
+    this.setState({date: data.date})
+  },
+  handleTime: function(data) {
+    this.setState({time: data.time})
+  },
+  handleInfo: function(data) {
+    this.setState({ info: { name: data.name, phone: data.phone, email: data.email }})
+  },
+  render: function() {
+    if(this.props.display === true) {
+      return(
+        <ReactCSSTransitionGroup transitionName="example" transitionAppear={true}>
+          <div className='panel-group' id='accordion' role='tablist' aria-multiselectable='true'>
+            <CalendarPanel availableDates={this.state.availableDates} availableTimes={this.state.availableTimes} onDateClick={this.handleDate} onTimeClick={this.handleTime}/>
+            <InfoPanel onClick={this.handleInfo} />
+            <ConfirmPanel date={this.state.date} time={this.state.time} info={this.state.info} />
+          </div>
+        </ReactCSSTransitionGroup>
+    )} else {
+      return(false);
+    }
+  }
+});
+
+var CalendarPanel = React.createClass({
+  getInitialState: function() {
+    return{availableDate: '', isOpen: false}
+  },
+  handleDateClick: function(i) {
+    this.props.onDateClick({date: this.props.availableDates[i]['day']});
+    this.setState({availableDate: i})
+  },
+  handleTimeClick: function(time) {
+    this.props.onTimeClick({time: time});
+  },
+  render: function() {
+    return (
+      <div className='panel panel-default'>
+        <a id='calendar-panel' data-toggle='collapse' data-parent='#accordion' href='#collapseOne' aria-expanded='true' aria-controls='collapseOne'>
+          <div className='panel-heading' role='tab' id='calendar'>
+            <h1 className='panel-title'>Calendar</h1>
+          </div>
+        </a>
+        <div id='collapseOne' className='panel-collapse collapse' role='tabpanel' aria-labelledby='calendar'>
+          <div className='panel-body'>
+            <h6 className='text-muted calendar-heading'>DATES</h6>
+            {this.props.availableDates.map(function(date, i) {
+              return (
+                <AvailableDate date={date} onClick={this.handleDateClick.bind(this, i)} key={i} isSelected={this.state.availableDate === i ? true : false }/>
+              );
+            }, this)}
+          <TimeSlots availableTimes={this.props.availableTimes} onClick={this.handleTimeClick}/>
+          </div>
+        </div>
+      </div>
+    );
+  }
+});
+
+var AvailableDate = React.createClass({
+  handleClick: function(i) {
+    this.props.onClick({})
+  },
+  render: function() {
+    var cx = React.addons.classSet;
+    var classes = cx({
+      'thumbnail': true,
+      'dates': true,
+      'active': this.props.isSelected
+    })
+    return(
+      <div key={this.props.key} className='col-xs-1'><div onClick={this.handleClick.bind(this, this.props.key)} className={classes}>{this.props.date['displayDay']}</div></div>
+    )
+  }
+});
+
+var TimeSlots = React.createClass({
+  getInitialState: function() {
+    return{activeTimeSlot: ''}
+  },
+  handleClick: function(i) {
+    this.props.onClick({time: this.props.availableTimes[i]['timeSlot']});
+    this.setState({activeTimeSlot: i})
+  },
+  render: function() {
+    return (
+      <div>
+        <h6 className='text-muted calendar-heading'>AVAILABILITY</h6>
+        {this.props.availableTimes.map(function(time, i){
+          return (
+            <TimeSlot time={time} onClick={this.handleClick.bind(this, i)} key={i} isSelected={this.state.activeTimeSlot === i ? true : false }/>
+          )
+          
+        }, this)}
+      </div>
+    );
+  }
+});
+
+var TimeSlot = React.createClass({
+  handleClick: function(i) {
+    this.props.onClick({})
+  },
+  render: function() {
+    var cx = React.addons.classSet;
+    var classes = cx({
+      'thumbnail': true,
+      'times': true,
+      'active': this.props.isSelected
+    })
+    return(
+      <div className='col-xs-3'><div key={this.props.key} onClick={this.handleClick.bind(this, this.props.key)} className={classes}>{this.props.time['displayTime']}</div></div>
+    )
+  }
+})
+
+var InfoPanel = React.createClass({
+  handleInfo: function(event) {
+    event.preventDefault();
+    var name = React.findDOMNode(this.refs.name).value.trim();
+    var phone = React.findDOMNode(this.refs.phone).value.trim();
+    var email = React.findDOMNode(this.refs.email).value.trim();
+    this.props.onClick({name: name, phone: phone, email: email})
+  },
+
+  render: function() {
+    return (
+      <div className='panel panel-default'>
+        <a id='info-panel' data-toggle='collapse' data-parent='#accordion' href='#collapseTwo' aria-expanded='true' aria-controls='collapseOne'>
+          <div className='panel-heading' role='tab' id='calendar'>
+            <h1 className='panel-title'>Your Info</h1>
+          </div>
+        </a>
+        <div id='collapseTwo' className='panel-collapse collapse' role='tabpanel' aria-labelledby='calendar'>
+          <div className='panel-body'>
+            <p className='text-muted'>Please add your info.</p>
+            <div className='form-horizontal'>
+              <input id='name' className='form-control' ref='name' placeholder='Name'></input>
+              <input id='phone' className='form-control' ref='phone' placeholder='Phone number'></input>
+              <input id='email' className='form-control' ref='email' placeholder='Email address'></input>
+              <button id='info-btn' className='btn btn-block' onClick={this.handleInfo}>Next</button>
+            </div>
+          </div>
+        </div>
+      </div>
+    )
+  }
+});
+
+var ConfirmPanel = React.createClass({
+  render: function() {
+    return (
+      <div className='panel panel-default'>
+        <a id='info-panel' data-toggle='collapse' data-parent='#accordion' href='#collapseThree' aria-expanded='true' aria-controls='collapseOne'>
+          <div className='panel-heading' role='tab' id='calendar'>
+            <h1 className='panel-title'>Confirmation</h1>
+          </div>
+        </a>
+        <div id='collapseThree' className='panel-collapse collapse' role='tabpanel' aria-labelledby='calendar'>
+          <div className='panel-body' align='center'>
+            <h4>Thanks {this.props.info.name}!</h4>
+            <p className='text-muted'>We have you set for {this.props.date} at {this.props.time}</p>
+            <p className='text-muted'>Please confirm the info below is correct.</p>
+            <p>{"Phone: " + this.props.info.phone}</p>
+            <p>{"Email: " + this.props.info.email}</p>
+            <button id="confirm-btn" className="btn btn-block">Confirm?</button>
+          </div>
+        </div>
+      </div>
+    )
+  }
+});
 
 React.render(
   <Content/>, document.getElementById('content')
